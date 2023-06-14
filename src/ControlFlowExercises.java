837706d8fcf3bae9718ae9f184e9f63240ea4e0a
Johann Williams <johann.williams01@gmail.com>
2023-06-14 12:35:29 -0500

    completed last part of 1c refactoring 1a and 1b to use a for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -15,11 +15,27 @@ public class ControlFlowExercises {
 //            i -= 5;
 //        } while(i >= -10);
 
-        int i = 2;
-        do{
+//        int i = 2;
+//        do{
+//            System.out.println(i);
+//            i = (int)Math.pow(i,2);
+//        }while(i < 1000000);
+
+//        for(int i = 5; i <= 15; i++){
+//            System.out.print(i + " ");
+//        }
+
+//        for(int i = 0; i <= 100; i += 2){
+//            System.out.println(i);
+//        }
+
+//        for(int i = 100; i >= -10; i-=5){
+//            System.out.println(i);
+//        }
+
+        for(int i = 2; i < 1000000; i = (int)Math.pow(i,2)){
             System.out.println(i);
-            i = (int)Math.pow(i,2);
-        }while(i < 1000000);
+        }
 
     }
 }
