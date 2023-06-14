18db8e19906720edb1db96c2e28d9008f71e0cde
Johann Williams <johann.williams01@gmail.com>
2023-06-14 12:50:59 -0500

    completed part 2, Fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,6 +3,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
 
+        /**
+         * Part 1 a, b, and c
+
 //        int i = 5;
 //        while(i <= 15){
 //            System.out.print(i + " ");
@@ -33,8 +36,24 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for(int i = 2; i < 1000000; i = (int)Math.pow(i,2)){
-            System.out.println(i);
+//        for(int i = 2; i < 1000000; i = (int)Math.pow(i,2)){
+//            System.out.println(i);
+//        }
+
+        */
+
+        for(int i = 1; i <= 100; i++){
+            if(i % 5 == 0){
+                if(i % 3 == 0){
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Buzz");
+                }
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
 
     }
