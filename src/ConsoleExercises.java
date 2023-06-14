478193e25aa218c11a2cc4436c58e23c6343577e
Johann Williams <johann.williams01@gmail.com>
2023-06-14 10:38:22 -0500

    completed part 3 and did bonus

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f%n", pi);
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an Integer: ");
+        scanner.useDelimiter("\n");
+
+
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f%n", pi);
+
+
+        System.out.println("Enter an Integer: ");
         int userInt = scanner.nextInt(); // if no int is inputted, then an exception is thrown.
         System.out.println("userInt = " + userInt);
 
 
-        System.out.print("Enter 3 words: "); // if less than 3. system waits for another input. if more only first 3 are printed
+        System.out.println("Enter 3 words: "); // if less than 3. system waits for another input. if more only first 3 are printed
         String word1 = scanner.next();
         String word2 = scanner.next();
         String word3 = scanner.next();
@@ -21,5 +28,30 @@ public class ConsoleExercises {
         String sentience = scanner.nextLine();
         System.out.println("sentience:\n" + sentience);
 
+        System.out.println("Calculate area of room. Enter Length followed by the width");
+        int len = scanner.nextInt();
+        int width = scanner.nextInt();
+        int height = scanner.nextInt();
+        int area = (int)calcArea(len, width);
+        int perimeter = (int)calcPerimeter(len, width);
+        System.out.println("area = " + area);
+        System.out.println("perimeter = " + perimeter);
+
+        int volume = (int)calcVol(len, width, height);
+        System.out.println("volume = " + volume);
+
     }
+
+    public static double calcArea(double l, double w){
+        return l*w;
+    }
+
+    public static double calcPerimeter(double l, double w){
+        return ((l * 2) + (w * 2));
+    }
+
+    public static double calcVol(double l, double w, double h){
+        return l*w*h;
+    }
+
 }
