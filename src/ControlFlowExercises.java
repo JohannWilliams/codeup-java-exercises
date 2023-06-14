d47969a8969ec532ca1fa9b85c495f2533140a70
Johann Williams <johann.williams01@gmail.com>
2023-06-14 11:54:04 -0500

    completed part 1a, and first part of 1b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,6 +3,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
 
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+        int i = 0;
+        do{
+            i+=2;
+            System.out.println("i = " + i);
+        } while(i < 100);
 
     }
 }
