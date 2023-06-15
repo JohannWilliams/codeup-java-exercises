07ff8456df7a30f98e9df35004e775e9c76b6e16
Johann Williams <johann.williams01@gmail.com>
2023-06-15 14:29:54 -0500

    created a recusive multiplication that works with positive numbers

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,6 +4,7 @@ public class MethodsExercises {
         System.out.println("Addition(5,10) = " + Addition(5, 10));
         System.out.println("Subtraction(10, 5) = " + Subtraction(10, 5));
         System.out.println("Multiplication(5, 5) = " + Multiplication(5, 5));
+        System.out.println("Multiplication(25,5) = " + Multiplication(25, 5));
         System.out.println("Division(25, 5) = " + Division(25, 5));
         System.out.println("Modulus(5, 5) = " + Modulus(5, 5));
 
@@ -15,8 +16,22 @@ public class MethodsExercises {
     public static int Subtraction(int a, int b){
         return a - b;
     }
+//    public static int Multiplication(int a, int b){
+//        return a * b;
+//    }
+//    public static int Multiplication(int a, int b){
+//        int result = 0;
+//        for(int i = 0; i < b; i++){
+//            result += a;
+//        }
+//        return result;
+//    }
+
     public static int Multiplication(int a, int b){
-        return a * b;
+        if(--b != 0){
+            a += Multiplication(a, b);
+        }
+        return a;
     }
     public static int Division(int a, int b){
         int result = 0;
