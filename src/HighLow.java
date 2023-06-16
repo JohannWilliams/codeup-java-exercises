f551fb5a7a7327b7b6d01eae753b70871edce254
Johann Williams <johann.williams01@gmail.com>
2023-06-16 11:55:48 -0500

    initalaized part 5 of methods exercise. high low game

---
+++ src/HighLow.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HighLow {
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+
+
+    public static void main(String[] args) {
+
+    }
+}
