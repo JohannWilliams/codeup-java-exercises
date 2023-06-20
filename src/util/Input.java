20d4f2b9792410d6fba92cc76a544e5f39f48b2b
Johann Williams <johann.williams01@gmail.com>
2023-06-19 22:21:33 -0500

    completed input validation class

--- src/util/Input.java
+++ src/util/Input.java
@@ -8,4 +8,49 @@ public class Input {
     public Input(){
         this.scanner = new Scanner(System.in);
     }
+
+    public String getString(){
+        System.out.println("Enter a string!");
+        return this.scanner.nextLine();
+    }
+
+    public boolean yesNo(){
+        System.out.println("Enter Yes or No");
+        String yNStr = this.scanner.nextLine();
+        boolean yNBool = false;
+        if(yNStr.equalsIgnoreCase("yes") || yNStr.equalsIgnoreCase("y")){
+            yNBool = true;
+        }
+        return yNBool;
+    }
+
+    public int getInt(int min, int max){
+        int userInt = 0;
+        do {
+            System.out.printf("Enter a number between %d and %d\n", min, max);
+            userInt = Integer.parseInt(this.scanner.nextLine());
+        }while(userInt > max || userInt < min);
+
+        return userInt;
+    }
+
+    public int getInt(){
+        System.out.println("Enter an Integer!");
+        return Integer.parseInt(this.scanner.nextLine());
+    }
+
+    public double getDouble(double min, double max){
+        double userDouble = 0;
+        do {
+            System.out.printf("Enter a number between %f and %f\n", min, max);
+            userDouble = Double.parseDouble(this.scanner.nextLine());
+        }while(userDouble > max || userDouble < min);
+
+        return userDouble;
+    }
+
+    public double getDouble(){
+        System.out.println("Enter a Double Number!");
+        return Double.parseDouble(this.scanner.nextLine());
+    }
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,21 @@
+package util;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+        Input testInput = new Input();
+
+        System.out.println(testInput.getString());
+
+        System.out.println(testInput.yesNo());
+
+        System.out.println(testInput.getInt(5, 10));
+
+        System.out.println(testInput.getInt());
+
+        System.out.println(testInput.getDouble(10.5, 25.999));
+
+        System.out.println(testInput.getDouble());
+
+    }
+}
