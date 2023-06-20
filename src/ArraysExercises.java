92473918097b1d154d16bfbd6da9947b515b1d74
Johann Williams <johann.williams01@gmail.com>
2023-06-20 14:11:26 -0500

    comment for testing git pull

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,6 +4,6 @@ public class ArraysExercises {
     public static void main(String[] args) {
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers)); // cannot use just numbers because an Java is strictly typed. Array != String.
-
+// added a comment
     }
 }
