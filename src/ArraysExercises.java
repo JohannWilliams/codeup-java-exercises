2bff33eddd73a1e128175de22bb767099029f3fd
Johann Williams <johann.williams01@gmail.com>
2023-06-23 00:42:34 -0500

    added array of 3 people, made addPerson function to add a new Person to end of array.

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,6 +4,24 @@ public class ArraysExercises {
     public static void main(String[] args) {
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers)); // cannot use just numbers because an Java is strictly typed. Array != String.
-// added a comment
+
+        Person[] people = {new Person("John"), new Person("Doe"), new Person("Bob")};
+
+        for(Person p: people){
+            System.out.println(p.getName());
+        }
+
+        people = addPerson(people, new Person("Jane"));
+
+        for(Person p: people){
+            System.out.println(p.getName());
+        }
+    }
+
+
+    public static Person[] addPerson(Person[] people, Person p){
+        Person[] newPeople = Arrays.copyOf(people, people.length + 1);
+        newPeople[newPeople.length - 1] = p;
+        return newPeople;
     }
 }
