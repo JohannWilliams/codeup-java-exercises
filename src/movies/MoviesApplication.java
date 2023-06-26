32cd154eb06fa31701e77f77b74c4c379d82d71e
Johann Williams <johann.williams01@gmail.com>
2023-06-26 11:15:00 -0500

    added comments to movies application and refactored add Movies and created a new method to get user input for new movie

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,11 +1,12 @@
 package movies;
 import util.Input;
 
-public class MoviesApplication {
-    private static final Input userInput = new Input();
-    private static Movie[] mArray = MoviesArray.findAll();
+import java.util.Arrays;
 
-    private static final String[] action = {"exit", "all", "animated", "drama", "horror", "scifi", "add"};
+public class MoviesApplication {
+    private static final Input userInput = new Input(); // used to get User Inputs (Strings, Integers)
+    private static Movie[] mArray = MoviesArray.findAll(); // grabs Initial Movies List/Array
+    private static final String[] action = {"exit", "all", "animated", "drama", "horror", "scifi", "add"}; // used to search specific movie category and set new movie's category.
 
     public static void main(String[] args) {
         int userChoice;
@@ -15,17 +16,17 @@ public class MoviesApplication {
             if(userChoice < 0 || userChoice > (action.length - 1)){
                 System.out.printf("Invalid Input. Please use a number between 0 and %d.\n", (action.length - 1));
             } else if (userChoice == 6) {
-                System.out.println("userChoice = " + userChoice);
+                createMovie();
             } else {
                 displayMovies(action[userChoice]);
             }
 
         }while(userChoice != 0);
-
-
-
     }
 
+    /**
+     * Display the main menu for requested user input
+     */
     public static void displayUserOptionMenu(){
         System.out.println("\nWhat would you like to do?\n");
 
@@ -38,11 +39,22 @@ public class MoviesApplication {
         System.out.println("6 - add a movie to the list\n");
     }
 
+    /**
+     * prompts the user for an integer choice. returns the value
+     * of the number entered.
+     * @return user input
+     */
     public static int getUserInput(){
         System.out.print("Enter your choice: ");
         return userInput.getInt();
     }
 
+    /**
+     * takes in a String of category or all.
+     * if all is passed in, display all movies in list/array
+     * else display only the movies that match the category passed in
+     * @param selectedCategory
+     */
     public static void displayMovies(String selectedCategory){
         if(selectedCategory.equalsIgnoreCase("all")){
             for(Movie m : mArray){
@@ -57,7 +69,36 @@ public class MoviesApplication {
                 }
             }
         }
+    }
 
+    /**
+     * after prompting user for inputs, takes in a String for new movie name
+     * and an integer response for specific category for the new movie. passes
+     * Name and Cat into method for adding movie to current movie list/array
+     */
+    public static void createMovie(){
+        System.out.print("Enter New Movie's Name: ");
+        String newMovieName = userInput.getString();
+        System.out.println("What is the Movie's Category");
+        System.out.println("1 - animated");
+        System.out.println("2 - drama");
+        System.out.println("3 - horror");
+        System.out.println("4 - scifi");
+        int newMovieCatNum = userInput.getInt(1, 4);
 
+        addNewMovie(newMovieName, action[newMovieCatNum + 1]);
+    }
+
+    /**
+     * Create a new Movie with name and category, then adds that to the current
+     * movies list/array.
+     * @param name
+     * @param cat
+     */
+    public static void addNewMovie(String name, String cat){
+        Movie newMovie = new Movie(name, cat);
+
+        mArray = Arrays.copyOf(mArray, mArray.length + 1);
+        mArray[mArray.length - 1] = newMovie;
     }
 }
