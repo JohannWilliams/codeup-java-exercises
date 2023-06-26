185919020fdb88329a7b2401663c8f7b1ffdde60
Johann Williams <johann.williams01@gmail.com>
2023-06-26 12:24:31 -0500

    updated Square Class, defines global side, overrides get area and perimeter methods.

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,20 @@
 package shapes;
 
 public class Square extends Rectangle{
+    protected double side;
+
     public Square(double side) {
         super(side, side);
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(this.side, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
     }
 }
