d2116408adbb8e859f285d3e75c3ba878cb51dd9
Johann Williams <johann.williams01@gmail.com>
2023-06-27 12:47:05 -0500

    modified Rect class to extend Quad class. overrides get Perimeter and Area as well as set Length and Width methods

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,28 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
+public class Rectangle extends Quadrilateral{
 
-    public Rectangle(double len, double w){
-        this.length = len;
-        this.width = w;
+    public Rectangle(double length, double width) {
+        super(length, width);
     }
 
-    public double getArea(){
+    @Override
+    public double getPerimeter() {
+        return (2 * length) + (2 * width);
+    }
+
+    @Override
+    public double getArea() {
         return length * width;
     }
 
-    public double getPerimeter(){
-        return (2 * length) + (2 * width);
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
     }
 }
