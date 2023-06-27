1a931be9347a70748346a63bf3dcaf60be896424
Johann Williams <johann.williams01@gmail.com>
2023-06-27 16:04:59 -0500

    added functinality to disply all students github names and ask for user input

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -2,10 +2,12 @@ package grades;
 
 import java.util.HashMap;
 import java.util.Random;
+import util.Input;
 
 public class GradesApplication {
 
     private static final Random rand = new Random();
+    private static final Input userInput = new Input();
 
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
@@ -24,9 +26,28 @@ public class GradesApplication {
         students.put("DoeDDDoeDoe", newStudents[2]);
         students.put("PlainJane", newStudents[3]);
 
-        for (String gh : students.keySet()) {
-            System.out.println("key: " + gh + " value: " + students.get(gh));
-        }
+        System.out.println("Welcome!\n");
+
+        displayUsers(students);
+
+        String requestedStudent = askForStudentToGetDataOn();
+        System.out.println(requestedStudent);
 
     }
+
+    public static void displayUsers(HashMap<String, Student> students){
+
+        System.out.println("Here are the GitHub usernames of our students:\n");
+
+        for (String gh : students.keySet()) {
+            System.out.print("|" + gh + "| ");
+        }
+        System.out.println("\n");
+    }
+
+    public static String askForStudentToGetDataOn(){
+        System.out.print("What student would you like to see more information on?\n\n> ");
+        return userInput.getString();
+    }
+
 }
