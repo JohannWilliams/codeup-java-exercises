4f4149d7d1e6ebd67351f60e4d713f33196d78b3
Johann Williams <johann.williams01@gmail.com>
2023-06-27 12:51:02 -0500

    updated Shapes Test to test Rect and Square using Mesurable. Answered Exercise questions at bottom in comments.

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,14 +3,33 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
+        Measurable myShape = new Rectangle(4, 5);
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
 
-        System.out.println("box1's area = " + box1.getArea());
-        System.out.println("box1's perimeter = " + box1.getPerimeter());
+        myShape = new Square(5);
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+//        myShape.getLength(); // produces an error, because Measurable does not define getLength();
 
-        Rectangle box2 = new Square(5);
-
-        System.out.println("box2's area = " + box2.getArea());
-        System.out.println("box2's perimeter = " + box2.getPerimeter());
+        Quadrilateral myQuad = new Square(10);
+        System.out.println("myQuad.getArea() = " + myQuad.getArea());
+        System.out.println("myQuad.getPerimeter() = " + myQuad.getPerimeter());
+        System.out.println("myQuad.getLength() = " + myQuad.getLength());
     }
+
+
+    /**
+     * Answers to question in Exercises
+     * 1 - Why does the code fail to compile if you leave off the getPerimeter method in Rectangle?
+     *          getPerimeter is an abstract method from the Measurable Interface. Because of this,
+     *          getPerimeter has to be present in the classes that implement it. No exceptions
+     *
+     * 2 - What happens if you try to call the getLength or getWidth methods of the myShape variable? Why?
+     *          We will get a compile time error because we have a Variable of Measurable that holds a Rect
+     *          or Square, but Measurable does not have a method for setLength or setWidth Only methods
+     *          defined by Measurable can be used.
+     *          Lines 15 - 18 show that a variable of Quadrilateral with the value of a new Square Object
+     *          can use the getLength or getWidth methods.
+     */
 }
