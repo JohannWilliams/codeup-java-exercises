101e505ba61d23544432b2aa153a782750b21de7
Johann Williams <johann.williams01@gmail.com>
2023-06-27 11:10:41 -0500

    removed side property from Square. using length for get Area and Perimeter.

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,20 +1,18 @@
 package shapes;
 
 public class Square extends Rectangle{
-    protected double side;
 
     public Square(double side) {
         super(side, side);
-        this.side = side;
     }
 
     @Override
     public double getArea() {
-        return Math.pow(this.side, 2);
+        return Math.pow(length, 2);
     }
 
     @Override
     public double getPerimeter() {
-        return 4 * side;
+        return 4 * length;
     }
 }
