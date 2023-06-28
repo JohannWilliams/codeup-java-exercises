e95dbaba2e1d69c68bce39038e162a41bb96631c
Johann Williams <johann.williams01@gmail.com>
2023-06-27 23:51:13 -0500

    completed bonus 4 for grades app. can print out a CSV style table if use desires.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Random;
 import util.Input;
 
+import javax.naming.Name;
+
 public class GradesApplication {
 
     private static final Random rand = new Random();
@@ -41,6 +43,8 @@ public class GradesApplication {
                 displayAllStudentGrades(students);
             } else if(requestedStudent.equalsIgnoreCase("class") || requestedStudent.equalsIgnoreCase("c")){
                 displayClassAverage(students);
+            } else if(requestedStudent.equalsIgnoreCase("csv")){
+                displayCSVStyleData(students);
             } else {
                 System.out.printf("Sorry, no student found with the GitHub username of \"%s\".%n", requestedStudent);
             }
@@ -63,7 +67,7 @@ public class GradesApplication {
     }
 
     public static String askForStudentToGetDataOn(){
-        System.out.print("What student would you like information on?\nType (A)ll to see all students grades. (C)lass to see class average.\n\n> ");
+        System.out.print("What student would you like information on?\nType (A)ll to see all students grades. (C)lass to see class average.\n(CSV) to get Table Printout.\n\n> ");
         return userInput.getString();
     }
 
@@ -83,6 +87,19 @@ public class GradesApplication {
             totalAverage += students.get(s).getGradeAverage();
         }
         System.out.printf("Class Average: %.1f%n%n", totalAverage/students.size());
+    }
+
+    public static void displayCSVStyleData(HashMap<String, Student> students){
+        String nameString = "Name";
+        String ghNameString = "GitHub_Username";
+        String averageString = "Average";
+        String csvString = String.format("%-10s%-22s%-10s%n", nameString, ghNameString, averageString);
+
+        for(String s: students.keySet()){
+            csvString += String.format("%-10s%-22s%-10.1f%n", students.get(s).getName(), s, students.get(s).getGradeAverage());
+        }
+
+        System.out.println(csvString);
     }
 
 }
